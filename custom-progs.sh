bed73afa32a1a9d6b1d3c941aaf90ad63cb39331
Dschorim <Dschorim@users.noreply.github.com>
2022-03-29 20:38:20 +0200

    fixed rustup dependency

--- custom-progs.sh
+++ custom-progs.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # paru (aur wrapper)
-sudo pacman -S --needed base-devel
+sudo pacman -S --needed base-devel rustup
+rustup default stable
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
