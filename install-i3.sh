598127bdb4ccc348140ebc5476fe079a8c01b111
jrmbchtl <s8jmbech@stud.uni-saarland.de>
2022-04-09 12:40:20 +0200

    added oh my zsh

--- install-i3.sh
+++ install-i3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-paru -S --noconfirm i3 i3status-rust j4-dmenu-desktop i3lock alsa pulseaudio xorg-xinit
+paru -S --noconfirm i3 i3status-rust j4-dmenu-desktop dmenu i3lock alsa pulseaudio xorg-xinit ly ttf-dejavu
 
 mkdir -p ~/.config/i3
 cp ./dotfiles/i3.config ~/.config/i3/config
 
 mkdir -p ~/.config/i3status-rust
-cp ./dotfiles/i3status.toml ~/.config/i3status-rust/config
+cp ./dotfiles/i3status.toml ~/.config/i3status-rust/config.toml
--- install-zsh.sh
+++ install-zsh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-paru -S --noconfirm zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+paru -S --noconfirm zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting powerline-fonts
 
 cp ./dotfiles/zshrc ~/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+chsh -s /usr/bin/zsh
